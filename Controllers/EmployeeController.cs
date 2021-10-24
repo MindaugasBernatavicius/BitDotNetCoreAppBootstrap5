5ea9286d9f78297222fbc13a3a20a29ac9ea2b0d
Mindaugas <darbas.mindaugas@gmail.com>
2021-10-24 18:59:45 +0300

    Adding models and repos

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApp2.Models;
+using System.Collections.Generic;
+
+namespace MvcApp2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeRepository _employeeRepository;
+        public EmployeeController(EmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public string MyMethod()
+        {
+            return "I pink up the phone and say yellow!";
+        }
+
+        public IActionResult Index()
+        {
+            // Some/Index.cshtml
+            //return View("~/Views/ABC.cshtml");
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(_employeeRepository.GetAll());
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            //if (id == null) return NotFound(); // 404
+            //if (_employeeRepository.DeleteById((int)id))
+            //{
+            //    //return Ok(); // 200
+            //    //return View("List", _employees);
+            //    return RedirectToAction("List");
+            //}
+            //else
+            //{
+            //    return NotFound(); // 404
+            //}
+
+            return (id == null || !_employeeRepository.DeleteById((int)id)) 
+                ? NotFound() 
+                : RedirectToAction("List");
+        }
+    }
+}
---
+++ Models/EmployeeRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcApp2.Models
+{
+    public class EmployeeRepository
+    {
+        private static List<Employee> _employees;
+        public EmployeeRepository()
+        {
+            _employees = new List<Employee>(){
+                new Employee() { Id = 1, Name = "Mindaugas"},
+                new Employee() { Id = 2, Name = "Jonas"},
+                new Employee() { Id = 3, Name = "Petras"}
+            };
+        }
+        public List<Employee> GetAll()
+        {
+            return _employees;
+        }
+
+        public bool DeleteById(int id)
+        {
+            var e = _employees.First(e => e.Id == id);
+            return _employees.Remove(e);
+        }
+    }
+}
